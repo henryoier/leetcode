83aa2feede4943ab7c11da727c9ceeb3d05804fa
henryoier <sheng.qin@yale.edu>
2016-07-23 17:21:58 +0800

    Add the first problem

---
+++ 001_TwoSum/001_TwoSum.cpp
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: 001_TwoSum.cpp
+	> Author: Sheng Qin
+	> Mail: sheng.qin@yale.edu
+	> Created Time: Wed Jul 20 22:56:52 2016
+ ************************************************************************/
+
+#include<iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+           
+    }
+};
+
+int main(){
+
+}
