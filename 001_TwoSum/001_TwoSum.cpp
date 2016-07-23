89284cea74c78fdba84dc2a58f00d2adccd3a498
henryoier <sheng.qin@yale.edu>
2016-07-23 21:20:03 +0800

    Accepted problem 001

--- 001_TwoSum/001_TwoSum.cpp
+++ 001_TwoSum/001_TwoSum.cpp
@@ -10,10 +10,36 @@
 
 using namespace std;
 
-int main(){
-    
-    for(int i = 0; i < 20; i++ ){
-        cout << i << endl;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+
+        for(int i = 0; i < nums.size() - 1; i++)
+            for(int j = i + 1; j < nums.size(); j++)
+                if(nums[i] + nums[j] == target){
+                    result.push_back(i);
+                    result.push_back(j);
+                    return result;
+                }
     }
+};
+
+int main(){
+    int n, target,number;
+    vector<int> nums;
+    vector<int> result;
+
+    cin >> n >> target;
+    for(int i = 0; i < n; i++){
+        cin >> number;
+        nums.push_back(number);
+    }
+
+    Solution newSolution;
+    result = newSolution.twoSum(nums, target);
+    
+    cout << result[0] << ' ' << result[1] << endl;
+
     return 0;
 }    
