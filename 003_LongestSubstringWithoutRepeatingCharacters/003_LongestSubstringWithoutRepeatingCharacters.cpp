23e1a765aada2215d9f6302b4af8e82e8f15ab6a
henryoier <sheng.qin@yale.edu>
2016-07-29 22:48:42 +0800

    Slightly modified

---
+++ 003_LongestSubstringWithoutRepeatingCharacters/003_LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,28 @@
+/*************************************************************************
+	> File Name: 003_LongestSubstringWithoutRepeatingCharacters.cpp
+	> Author: Sheng Qin
+	> Mail: sheng.qin@yale.edu
+	> Created Time: Tue Jul 26 16:43:09 2016
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        return s.length;
+    }
+};
+
+int main(){
+    Solution newSolution;
+    string s;
+
+    cin >> s;
+    cout << newSolution.lengthOfLongestSubstring(s) << endl;
+
+    return 0;
+}
