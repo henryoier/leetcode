e09c3707b5f838e11d6720069bba3f503f75a5d6
henryoier <sheng.qin@yale.edu>
2016-08-02 00:10:46 +0800

    Solved problem 002 and add debug settings for vs code

--- 002_AddTwoNumbers/002_AddTwoNumbers.cpp
+++ 002_AddTwoNumbers/002_AddTwoNumbers.cpp
@@ -11,19 +11,66 @@
 using namespace std;
 
 struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() {};
-    ListNode(ListNode *a) : val(a->val), next(a->next){}
-    ListNode(int x) : val(x), next(NULL) {}
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
 };
 
-/*class Solution {
+class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode
+        ListNode *result, *now, *last;
+        int carry = 0;
+        bool head = true; 
+        while(l1 != NULL && l2 != NULL){
+            now = new ListNode(0); // The only initialization function
+            now->val = l1->val + l2->val + carry;
+            carry = now->val / 10;
+            now->val = now->val % 10;
+            
+            if(head){
+                result = now;
+                last = now;
+                head = false;
+            } else{
+                last->next = now;
+                last = now;
+            }
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        while(l1 != NULL){
+            now = new ListNode(0);
+            now->val = l1->val + carry;
+            carry = now->val / 10;
+            now->val = now->val % 10;
+
+            last->next = now;
+            last = now;
+
+            l1 = l1->next;
+        }
+
+        while(l2 != NULL){
+            now = new ListNode(0);
+            now->val = l2->val + carry;
+            carry = now->val / 10;
+            now->val = now->val % 10;
+
+            last->next = now;
+            last = now;
+
+            l2 = l2->next;
+        }
+
+        if(carry != 0){
+            now = new ListNode(carry);
+            last->next = now;
+        }
+
+        return result;
     }
-};*/
+};
 
 int main(){
     int len1,len2,n;
@@ -33,10 +80,10 @@ int main(){
 
     for(int i = 0; i < len1; i++){
         cin >> n;
-        ListNode *now(n);
+        ListNode* now;
+        now = new ListNode(n);
         if( i == 0 ){
             l1 = now;
-            last->next = now;
             last = now;
         }else{
             last->next = now;
@@ -44,12 +91,12 @@ int main(){
         }
     }
 
-     for(int i = 0; i < len2; i++){
+    for(int i = 0; i < len2; i++){
         cin >> n;
-        ListNode *now(n);
+        ListNode* now;
+        now = new ListNode(n);
         if( i == 0 ){
             l2 = now;
-            last->next = now;
             last = now;
         }else{
             last->next = now;
@@ -57,9 +104,13 @@ int main(){
         }
     }
 
-    ListNode *now = l1;
-    while(now->next != NULL){
-        cout << now->val;
-        now = now->next;
+    Solution newSolution;
+    ListNode *result = newSolution.addTwoNumbers(l1, l2);
+
+    while(result != NULL){
+        cout << result->val;
+        result = result->next;
     }
+
+    return 0;
 }
