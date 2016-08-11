5f41d3872d2ee35be41b3d4c74aba38e929aad74
henryoier <sheng.qin@yale.edu>
2016-08-11 16:57:03 +0800

    Solved problem 003

--- 003_LongestSubstringWithoutRepeatingCharacters/003_LongestSubstringWithoutRepeatingCharacters.cpp
+++ 003_LongestSubstringWithoutRepeatingCharacters/003_LongestSubstringWithoutRepeatingCharacters.cpp
@@ -7,13 +7,69 @@
 
 #include<iostream>
 #include<string>
+#include<algorithm>
 
 using namespace std;
 
-class Solution {
+/*class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        return s.length();
+        if(s.length() == 0)
+            return 0;
+        
+        int head = 1, tail = s.length();
+        int midMax = 1;
+        while(head < tail){       
+            int mid = (head + tail + 1) / 2;
+            int index = 0;
+            bool unique = false;
+            while(index + mid <= s.length()){ 
+                if(checkUnique(s.substr(index, mid))){
+                    unique = true;
+                    break;
+                } else index++;            
+            }
+            if(unique){
+                if(mid > midMax)
+                    midMax = mid;
+                head = mid;
+            } else {
+                tail = mid - 1;
+            }
+        }
+        return midMax;
+    }
+
+private:
+    bool checkUnique(string str){
+        sort(str.begin(), str.end());
+        for(auto s = str.begin(); s < str.end() - 1; s++){
+            if(*s == *(s+1))
+                return false;
+        } 
+        return true;
+    }
+};*/
+
+class Solution{
+public:
+    int lengthOfLongestSubstring(string s){
+        const int ASCII_MAX = 255;
+        int last[ASCII_MAX];
+        int start = 0, maxLen = 0;
+        fill(last, last + ASCII_MAX, -1);
+
+        for(int i = 0; i < s.size(); i++){
+            if(last[s[i]] >= start){
+                if(i - start > maxLen)
+                    maxLen = i - start;
+                start = last[s[i]] + 1;
+            }
+            last[s[i]] = i;
+        }
+        if(s.size() - start > maxLen)
+            maxLen = s.size() - start;
+        return maxLen;
     }
 };
 
